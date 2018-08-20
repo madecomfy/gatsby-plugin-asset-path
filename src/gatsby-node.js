5f54e5eb8bd54362933af22777bda88995e26d77
Ryan Chung <ryan@madecomfy.com.au>
2018-08-20 17:20:51 +1000

    Fixed public path to be relative

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -8,8 +8,8 @@ import * as path from "path"
  * @see {@link https://github.com/gatsbyjs/gatsby/blob/v2.0.0-alpha.20/packages/gatsby/src/utils/webpack.config.js#L240}
  * @param {*} param0
  */
-export const onCreateWebpackConfig = ({ stage, actions }, { options }) => {
-  if (options.removeMapFiles === true && stage === "build-javascript") {
+export const onCreateWebpackConfig = ({ stage, actions }, { removeMapFiles }) => {
+  if (removeMapFiles === true && stage === "build-javascript") {
     actions.setWebpackConfig({
       devtool: false
     })
@@ -21,28 +21,28 @@ export const onCreateWebpackConfig = ({ stage, actions }, { options }) => {
  * @see {@link https://next.gatsbyjs.org/docs/node-apis/#onPostBuild}
  */
 export const onPostBuild = async ({ pathPrefix }) => {
-  const publicFolder = path.resolve(__dirname, "public")
-  const assetFolder = path.resolve(publicFolder, `.${pathPrefix}`)
+  const publicFolder = "./public"
+  const assetFolder = path.join(publicFolder, `.${pathPrefix}`)
 
   // Move js and css files
   const files = fs.readdirSync(publicFolder)
   await Promise.all(files.map((file) => {
     if (/.*\.(js|css)$/.test(file)) {
-      const currentPath = path.resolve(publicFolder, file)
-      const newPath = path.resolve(assetFolder, file)
+      const currentPath = path.join(publicFolder, file)
+      const newPath = path.join(assetFolder, file)
       return fs.move(currentPath, newPath)
     }
   }))
 
   // Move data files
   const staticFolder = "static"
-  const currentStaticPath = path.resolve(publicFolder, staticFolder)
-  const newStaticPath = path.resolve(assetFolder, staticFolder)
+  const currentStaticPath = path.join(publicFolder, staticFolder)
+  const newStaticPath = path.join(assetFolder, staticFolder)
   await fs.move(currentStaticPath, newStaticPath)
 
   // Copy sitemap.xml
   const sitemap = "sitemap.xml"
-  const currentSitemapPath = path.resolve(publicFolder, sitemap)
-  const newSitemapPath = path.resolve(assetFolder, sitemap)
+  const currentSitemapPath = path.join(publicFolder, sitemap)
+  const newSitemapPath = path.join(assetFolder, sitemap)
   await fs.copy(currentSitemapPath, newSitemapPath)
 }
