217487fca1de4bc74350140827a68b62a36daef3
raurir <raurir@gmail.com>
2019-07-15 15:42:25 +1000

    update to use gatsbys assetPrefix

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,5 +1,5 @@
-import * as fs from "fs-extra"
-import * as path from "path"
+import * as fs from "fs-extra";
+import * as path from "path";
 
 /**
  * Removes the .map files
@@ -8,41 +8,49 @@ import * as path from "path"
  * @see {@link https://github.com/gatsbyjs/gatsby/blob/v2.0.0-alpha.20/packages/gatsby/src/utils/webpack.config.js#L240}
  * @param {*} param0
  */
-export const onCreateWebpackConfig = ({ stage, actions }, { removeMapFiles }) => {
+export const onCreateWebpackConfig = (
+  { stage, actions },
+  { removeMapFiles }
+) => {
   if (removeMapFiles === true && stage === "build-javascript") {
     actions.setWebpackConfig({
       devtool: false
-    })
+    });
   }
-}
+};
 
 /**
  * Moves all js and css files into timestamp-named folder
  * @see {@link https://next.gatsbyjs.org/docs/node-apis/#onPostBuild}
  */
 export const onPostBuild = async ({ pathPrefix }) => {
-  const publicFolder = "./public"
-  const assetFolder = path.join(publicFolder, `.${pathPrefix}`)
+  const publicFolder = "./public";
+  const assetFolder = path.join(publicFolder, `.${pathPrefix}`);
 
-  // Move js and css files
-  const files = fs.readdirSync(publicFolder)
-  await Promise.all(files.map((file) => {
-    if (/.*\.(js|css)$/.test(file)) {
-      const currentPath = path.join(publicFolder, file)
-      const newPath = path.join(assetFolder, file)
-      return fs.move(currentPath, newPath)
-    }
-  }))
+  const copy = fileOrFolder => {
+    const currentPath = path.join(publicFolder, fileOrFolder);
+    const newPath = path.join(assetFolder, fileOrFolder);
+    return fs.copy(currentPath, newPath);
+  };
+  const move = fileOrFolder => {
+    const currentPath = path.join(publicFolder, fileOrFolder);
+    const newPath = path.join(assetFolder, fileOrFolder);
+    return fs.move(currentPath, newPath);
+  };
 
-  // Move data files
-  const staticFolder = "static"
-  const currentStaticPath = path.join(publicFolder, staticFolder)
-  const newStaticPath = path.join(assetFolder, staticFolder)
-  await fs.move(currentStaticPath, newStaticPath)
+  // Move css,js and webmanifest files
+  const files = fs.readdirSync(publicFolder);
+  await Promise.all(
+    files.map(file => {
+      if (/.*\.(js|css|webmanifest)$/.test(file)) {
+        return move(file);
+      }
+    })
+  );
 
-  // Copy sitemap.xml
-  const sitemap = "sitemap.xml"
-  const currentSitemapPath = path.join(publicFolder, sitemap)
-  const newSitemapPath = path.join(assetFolder, sitemap)
-  await fs.copy(currentSitemapPath, newSitemapPath)
-}
+  // Move statics data and icons
+  await Promise.all(["static", "icons"].map(move));
+
+  // Copy page data and sitemap
+  await Promise.all(["page-data", "sitemap.xml"].map(copy));
+};
