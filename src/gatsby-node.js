771262aa48a36f16308c3d56153cbc1bd7b8ec22
Hannes Elvemyr <hannes@accodeing.com>
2019-10-14 02:07:06 +0200

    Moves page-data instead of copying it (#21)
    * Moves page-data instead of copying it
    
    * Removes unused copy method
    
    * Refactors code that collects files/folders to move

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -27,32 +27,21 @@ export const onPostBuild = async ({ pathPrefix }, { additionalFiles = [] }) => {
   const publicFolder = "./public";
   const assetFolder = path.join(publicFolder, `.${pathPrefix}`);
 
-  const copy = (fileOrFolder) => {
-    const currentPath = path.join(publicFolder, fileOrFolder);
-    const newPath = path.join(assetFolder, fileOrFolder);
-    return fs.copy(currentPath, newPath);
-  };
   const move = (fileOrFolder) => {
     const currentPath = path.join(publicFolder, fileOrFolder);
     const newPath = path.join(assetFolder, fileOrFolder);
     return fs.move(currentPath, newPath);
   };
 
-  // Move css and js
-  const files = fs.readdirSync(publicFolder);
-  await Promise.all(
-    files.map((file) => {
-      if (/.*\.(js|css)$/.test(file)) {
-        return move(file);
-      }
-    }),
-  );
+  const filterFilesIn = (folder) =>
+    fs.readdirSync(folder).filter((file) => /.*\.(js|css)$/.test(file));
 
-  await Promise.all(additionalFiles.map((file) => move(file)));
+  const filesInPublicFolder = filterFilesIn(publicFolder);
+  const directories = ["static", "icons", "page-data"];
+  const thingsToMove = directories
+    .concat(filesInPublicFolder)
+    .concat(additionalFiles);
 
-  // Move statics data and icons
-  await Promise.all(["static", "icons"].map(move));
-
-  // Copy page data
-  await Promise.all(["page-data"].map(copy));
+  // Move files and directories
+  await Promise.all(thingsToMove.map(move));
 };
